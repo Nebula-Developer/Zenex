5085455b6b65cb7d3f563cb3946d253bb8005e3c
NebulaDev <nebuladev.contact@gmail.com>
2023-01-12 22:33:59 +1300

    Swap to `array` and support `encrypt` in accounts

--- test/account.js
+++ test/account.js
@@ -3,11 +3,24 @@ const zenex = require('..');
 var app = zenex();
 
 var accounts = app.genAccountSystem({
-    name: 'test'
+    name: 'test',
+    encrypt: false
 });
 
 app.createServer();
 app.useStatic('test');
+
+console.log(accounts.getAccounts());
+
+console.log(accounts.runForAccounts(accounts.getManyFromObject({
+    username: 'test'
+}), (account) => {
+    console.log("Got", account);
+}));
+accounts.addAccount({
+    username: 'test',
+    password: 'test'
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
