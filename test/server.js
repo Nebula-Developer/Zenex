80039788d926198bb772402aeaeef27f6eddf44d
NebulaDev <nebuladev.contact@gmail.com>
2023-01-25 15:56:49 +1300

    Fix non-null check for `ZenexConfig.errors`

--- test/server.js
+++ test/server.js
@@ -2,12 +2,6 @@ const zenex = require('..');
 const { ZenexHandler } = require('../lib/types');
 
 var app = zenex({
-    errors: {
-        notFound: (req, res) => {
-            res.writeHead(404);
-            res.end('404 Page Not Found');
-        }
-    },
     useNodeStatic: true
 });
 
